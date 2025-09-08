6ff75e95f74cc77e02287b4e6351bd4050194bc7
forzayt <vishnusanthoshvr@gmail.com>
2025-09-08 23:38:21 +0530

    first commit

---
+++ addon.js
@@ -0,0 +1,81 @@
+const { addonBuilder } = require("stremio-addon-sdk");
+const streamsData = require("./streamsData");
+
+const manifest = { 
+    "id": "org.stremio.helloworld",
+    "version": "1.0.0",
+
+    "name": "Hello World Addon",
+    "description": "Sample addon providing a few public domain movies",
+
+    // set what type of resources we will return
+    "resources": [
+        "catalog",
+        "stream"
+    ],
+
+    "types": ["movie", "series"], // your add-on will be preferred for those content types
+
+    // set catalogs, we'll be making 2 catalogs in this case, 1 for movies and 1 for series
+    "catalogs": [
+        {
+            type: 'movie',
+            id: 'helloworldmovies'
+        },
+        {
+            type: 'series',
+            id: 'helloworldseries'
+        }
+    ],
+
+    // prefix of item IDs (ie: "tt0032138")
+    "idPrefixes": [ "tt" ]
+
+};
+
+// Build dataset using only IMDb ID and direct MP4 URL from streamsData
+const dataset = Object.fromEntries(
+    Object.entries(streamsData).map(([imdbId, mp4Url]) => [
+        imdbId,
+        { name: imdbId, type: "movie", url: mp4Url }
+    ])
+);
+
+// Note: dataset is now simplified to only direct MP4 URL streams
+
+const builder = new addonBuilder(manifest);
+
+// Streams handler
+builder.defineStreamHandler(function(args) {
+    if (dataset[args.id]) {
+        return Promise.resolve({ streams: [dataset[args.id]] });
+    } else {
+        return Promise.resolve({ streams: [] });
+    }
+})
+
+const METAHUB_URL = "https://images.metahub.space"
+
+const generateMetaPreview = function(value, key) {
+    // To provide basic meta for our movies for the catalog
+    // we'll fetch the poster from Stremio's MetaHub
+    // see https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/meta.md#meta-preview-object
+    const imdbId = key.split(":")[0]
+    return {
+        id: imdbId,
+        type: value.type,
+        name: value.name,
+        poster: METAHUB_URL+"/poster/medium/"+imdbId+"/img",
+    }
+}
+
+builder.defineCatalogHandler(function(args, cb) {
+    // filter the dataset object and only take the requested type
+    const metas = Object.entries(dataset)
+	.filter(([_, value]) => value.type === args.type)
+	.map(([key, value]) => generateMetaPreview(value, key))
+
+    return Promise.resolve({ metas: metas })
+})
+
+module.exports = builder.getInterface()
---
+++ streamsData.js
@@ -0,0 +1,10 @@
+// Map IMDb IDs to direct MP4 URLs.
+// Edit this file to add or change your items.
+// Example:
+// "tt1234567": "https://example.com/video.mp4"
+
+module.exports = {
+    "tt4679210": "https://archive.org/download/702434premam125/702434premam125.mp4"
+};
+
+
