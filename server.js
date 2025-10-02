8f487fe7b191239219b04ae9da8fb5f4a579eff3
forzayt <vishnusanthoshvr@gmail.com>
2025-10-02 19:11:05 +0530

    q

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 const { serveHTTP } = require("stremio-addon-sdk");
-const { syncAllStreams } = require("./discordWebhook");
 
 const addonInterface = require("./addon");
-
-// Start the server
-serveHTTP(addonInterface, { port: 7001 });
-
-// Automatically sync all streams to Discord on startup
-syncAllStreams();
+serveHTTP(addonInterface, { port: 7000 });
 
