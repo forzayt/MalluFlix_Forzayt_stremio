8bef70e8db6727c2560a68ac2d7de674c0953481
forzayt <vishnusanthoshvr@gmail.com>
2025-10-12 07:04:13 +0530

    w

--- livetvData.js
+++ livetvData.js
@@ -11,12 +11,29 @@ const liveTvChannels = [
         group: "News",
         category: "News"
     },
+
+
+
+    
     {
         id: "asianet",
         name: "Asianet",
         title: "Asianet",
         url: "http://217.20.112.199/asianet/index.m3u8",
         logo: "https://yt3.googleusercontent.com/rD84Z2RyqjpzQ7xYvOl4Rcn_MnAny8VvcF5a0Iz76A0EnYTRHVYU-SLIIxR_a9dv_95QNCl76WY=s900-c-k-c0x00ffffff-no-rj",
+        group: "News",
+        category: "News"
+    },
+
+
+
+
+    {
+        id: "surya",
+        name: "Surya TV",
+        title: "Surya TV",
+        url: "https://livestream10.sunnxt.com/DolbyVision/SuryaTV_HDR/SuryaTV_HDR_Endpoints/SuryaTV-HDR10-IN-index.m3u8",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvnBdNqlDa6Y9iVaEgsuETggn113rLeClOSQ&s",
         group: "News",
         category: "News"
     },
