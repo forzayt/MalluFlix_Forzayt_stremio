5a6129695f7025a366a2bafbda6dd15c97f171e2
forzayt <vishnusanthoshvr@gmail.com>
2025-10-18 11:39:22 +0530

    q

--- livetvData.js
+++ livetvData.js
@@ -45,7 +45,7 @@ const liveTvChannels = [
         name: "ULTIMATE BODY TAPE | Watch Party | Best of Miami Swim Week",
         title: "ULTIMATE BODY TAPE | Watch Party | Best of Miami Swim Week",
         url: "https://hugh.cdn.rumble.cloud/live/t5qc33px/slot-136/w7m3-n0o0/chunklist.m3u8",
-        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz7m8lIJ1q-5N63Y83Omhcrxq-PTxx4mSRlg&s",
+        logo: "https://hugh.cdn.rumble.cloud/video/z8/m/m/-/v/mm-va.baa-SPLASHME-s8jgyf.jpeg",
         group: "News",
         category: "News"
     },
