3fb09138a0efca6505baf0b49fdc48cb0927cb74
forzayt <vishnusanthoshvr@gmail.com>
2025-12-25 21:03:50 +0530

    Refactor: Comment out movie entries in movieData.js

--- movieData.js
+++ movieData.js
@@ -1,12 +1,12 @@
 const movies = [
-    {
-        id: "tt36302733",                                              // Dise Irae
-        url: "https://rumble.com/hls-vod/711k7m/playlist.m3u8"
-    },
-    {
-        id: "tt3668162",                                           // Banglore Days
-        url: "https://rumble.com/hls-vod/711i8m/playlist.m3u8"
-    }
+    // {
+    //     id: "tt36302733",                                              // Dise Irae
+    //     url: "https://rumble.com/hls-vod/711k7m/playlist.m3u8"
+    // },
+    // {
+    //     id: "tt3668162",                                           // Banglore Days
+    //     url: "https://rumble.com/hls-vod/711i8m/playlist.m3u8"
+    // }
 ];
 
 module.exports = movies;
